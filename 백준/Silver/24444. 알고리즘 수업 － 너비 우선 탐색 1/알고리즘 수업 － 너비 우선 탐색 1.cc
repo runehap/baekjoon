51d08c7d5ff0cfd87ca4c2d7ef378aaa92e27a81
runehap <110074284+runehap@users.noreply.github.com>
2023-01-25 10:31:04 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 216 ms, Memory: 9532 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+vector<int> v[100001];
+queue<int> q;
+bool check[100001];
+int result[100001];
+int cnt = 0;
+
+void bfs(int r)
+{
+	result[r] = ++cnt;
+	check[r] = true;
+	q.push(r);
+	while (!q.empty())
+	{
+		int qq = q.front();
+		q.pop();
+		for (auto i : v[qq])
+		{
+			if (check[i] == false)
+			{
+				check[i] = true;
+				result[i] = ++cnt;
+				q.push(i);
+			}
+		}
+	}
+	
+}
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	for (int i = 0; i < b; i++)
+	{
+		int g, h;
+		cin >> g >> h;
+		v[g].push_back(h);
+		v[h].push_back(g);
+	}
+	for (int i = 1; i <= a; i++)
+	{
+		sort(v[i].begin(), v[i].end());
+	}
+	bfs(c);
+	for (int i = 1; i <= a; i++)
+	{
+		printf("%d\n", result[i]);
+	}
+	
+
+	return 0;
+}
