b0bb3ac5543a3c9efba4419a70c5b9ba8980bd04
runehap <110074284+runehap@users.noreply.github.com>
2023-01-30 09:39:35 +0900

    Update 색종이 만들기.cc

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -2,27 +2,27 @@
 
 using namespace std;
 
-int arr[128][128];
-int ans[2];
+int arr[128][128]; //처음 주어지는 종이
+int ans[2]; // 색종이 개수별 카운트
 
 void recu(int x, int y, int s)
 {
-	bool check = true;
-	for (int i = 0; i < s; i++)
+	bool check = true; // s*s 사이즈의 색종이 인가?
+	for (int i = 0; i < s; i++) // 세로 사이즈 만큼 반복
 	{
-		for (int j = 0; j < s; j++)
+		for (int j = 0; j < s; j++)//가로 사이즈 만큼 반복
 		{
-			if (arr[x + i][y + j] != arr[x][y])
+			if (arr[x + i][y + j] != arr[x][y]) // 만약 s*s 사이즈 안에 서로 다른 색이 존재하는 경우
 			{
-				check = false;
-				break;
+				check = false; // s*s 사이즈의 색종이가 아니다
+				break;// 반복 탈충
 			}
-			if (!check)
-				break;
+			if (!check)// 체크가 false 인경우
+				break; // 모든 반복 탈출
 		}
 	}
-	if (check)
-		ans[arr[x][y]]++;
+	if (check)//만약 체크가 true인 경우
+		ans[arr[x][y]]++;//색종이 개수 +1
 	else
 	{
 		int new_size = s / 2;
