a9b592e6915a68554926538d3d652e9245a19858
runehap <110074284+runehap@users.noreply.github.com>
2024-05-30 09:13:25 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+priority_queue<int,vector<int>,greater<int>> pq;
+
+int main(){
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+int n;
+cin >> n;
+while(n--)
+{
+    int num;
+    cin >> num;
+
+    if(!num){
+        if(!pq.empty())
+        {
+            cout << pq.top() << "\n";
+            pq.pop();
+        }
+        else{
+            cout << 0 << "\n";
+        }
+    }
+    else{
+        pq.push(num);
+    }
+}
+
+
+    return 0;
+}
