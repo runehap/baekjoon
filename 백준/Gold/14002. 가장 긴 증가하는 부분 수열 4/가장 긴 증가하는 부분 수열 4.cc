51f82957ba48f74c30c5397078e74e2e35ca9d70
runehap <110074284+runehap@users.noreply.github.com>
2024-06-10 14:14:00 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int arr[1002];
+int dp[1002];
+int q[1002];
+stack<int> result;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    int Max = 1;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        cin >> arr[i];
+        dp[i] = 1;
+        for(int j=0;j<i;j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                if(dp[i] <= dp[j])
+                {
+                    dp[i] = dp[j]+1;
+                    q[i] = j;   
+                }
+            }
+            Max = max(dp[i], Max);
+        }
+    }
+    cout << Max <<"\n";
+
+    for(int i=n-1;i>=0;i--)
+    {
+        if(dp[i] == Max)
+        {
+            result.push(arr[i]);
+            Max--;
+        }
+
+        if(Max <= 0)break;
+
+    }
+
+    while(!result.empty())
+    {
+        cout << result.top() << " ";
+        result.pop();
+    }
+
+    return 0;
+}
